caa2f3765e2c54249cb604c257a853f485eb510a
Kevin <hihellohi610@gmail.com>
2017-09-12 18:01:13 +1000

    test0.5

--- Graph.h
+++ Graph.h
@@ -2,15 +2,28 @@
 #define GDWG
 
 #include <map>
+#include <set>
 #include <memory>
+#include <vector>
+#include <stdexcept>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
 
 namespace gdwg {
 	template <typename N, typename E>
 	class Graph {
 		private:
+			struct Node;
+
+			class compareNode {
+				public:
+					bool operator()(const std::weak_ptr<Node> &a, const std::weak_ptr<Node> &b);
+			};
+
 			struct Node {
 				N data_;
-				std::map<E, std::map<N, std::shared_ptr<Node>>> nodes_;
+				std::map<E, std::set<std::weak_ptr<Node>, compareNode>> neighbours_;
 				unsigned int degree_;
 
 				Node(const N &data) : data_{data} {}
